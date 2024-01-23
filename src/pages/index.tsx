6455c5c20e30f9094a0efa65f171372e7bb1a6e5
Yechan Jung <asdxcv1998@gmail.com>
2024-01-23 22:11:58 +0900

    react-quill 사용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import { use, useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function Page() {
+  const router = useRouter();
   return (
     <>
       <h1>Initial Page</h1>
+      <button onClick={() => router.push("/post")}>Go to post</button>
     </>
   );
 }
--- src/pages/post.tsx
+++ src/pages/post.tsx
@@ -1,7 +1,31 @@
+import dynamic from "next/dynamic";
+import { useState } from "react";
+import "react-quill/dist/quill.snow.css";
+
+const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
+
 export default function Post() {
+  const [value, setValue] = useState("");
+
   return (
     <>
       <h1>Post</h1>
+      <QuillEditor value={value} setValue={setValue} />
+      <button
+        onClick={() => {
+          save(value);
+        }}
+      >
+        Submit
+      </button>
     </>
   );
+}
+
+function QuillEditor({ value, setValue }: any) {
+  return <ReactQuill theme="snow" value={value} onChange={setValue} />;
+}
+
+function save(value: string) {
+  console.log(value);
 }
