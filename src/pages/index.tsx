56d5f5bab392db9adf1f512469216636840d63b4
Yechan Jung <asdxcv1998@gmail.com>
2024-01-23 21:12:21 +0900

    editorJS 테스트

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,31 +1,9 @@
 import { use, useEffect, useState } from "react";
 
-import testFetch from "@/apis/test";
-
 export default function Page() {
   return (
     <>
       <h1>Initial Page</h1>
-      <h3>TC</h3>
-      <TestComponent />
     </>
   );
-}
-
-function TestComponent() {
-  const [data, setData] = useState<{ data: string | null; id: number }[]>([]);
-  // set data from testFetch
-  useEffect(() => {
-    testFetch().then((res) => {
-      setData(res);
-    });
-  }, []);
-
-  console.log(data);
-
-  const list = data.map((item) => {
-    return <li key={item.id}>{item.data}</li>;
-  });
-
-  return <>{list}</>;
 }
