c4deaeaa6e303a92b70c2e22bf42d89e6cc0bb34
Yechan Jung <asdxcv1998@gmail.com>
2024-02-19 22:44:42 +0900

    리스트 뷰 수정 (#3)
    * Add react-infinite-scroll-component dependency
    
    * Refactor getPosts function and update image rendering in index and post pages
    
    * Add date-fns library and format date in list view

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,65 @@
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import Image from "next/image";
+import { formatDistanceToNow } from "date-fns";
+import { ko } from "date-fns/locale";
 
-import { getPosts } from "../apis/getPosts";
-import { Post } from "@/types/types";
+import { ListElement, getPosts } from "../apis/getPosts";
 
 export default function Page() {
   const router = useRouter();
-  const [posts, setPosts] = useState<Post[]>([]);
+  const [listElements, setListElements] = useState<ListElement[]>([]);
 
   useEffect(() => {
-    getPosts().then((posts) => setPosts(posts));
+    getPosts(0, 10).then((postImageData) => {
+      if (postImageData === null) {
+        throw new Error("postImageData is null");
+      }
+      return setListElements(postImageData);
+    });
   }, []);
 
   const goPost = (id: number) => {
     router.push(`/post/${id}`);
   };
 
+  const formatTimeAgo = (date: string) => {
+    return formatDistanceToNow(new Date(date), { addSuffix: true, locale: ko });
+  };
+
   return (
     <>
-      <div className="h-screen mx-auto mt-10 mb-10">
-        <h1 className=" text-3xl m-10">List</h1>
+      <div className="mx-auto bg-gray-100 min-h-screen p-10">
+        <h1 className="text-3xl pb-4 text-blue-500">Logo Here</h1>
         <div>
-          {posts.map((post) => (
-            <li className="border border-black list-none p-2 flex justify-between" key={post.id}>
-              <a onClick={() => goPost(post.id)}>
-                <h2 className="text-xl">{post.title}</h2>
-                <p className="text-sm">{post.created_at}</p>
+          {listElements.map((listElement: ListElement) => (
+            <li
+              className="border border-black list-none p-2 flex justify-between bg-white mb-4 rounded shadow"
+              key={listElement.id}
+            >
+              <a className="w-full text-gray-700" onClick={() => goPost(listElement.id)}>
+                <h2 className="text-xl">{listElement.title}</h2>
+                <p className="text-sm text-gray-500">{formatTimeAgo(listElement.created_at ?? "")}</p>
+                {listElement.image.length > 0 && (
+                  <div className="flex justify-center">
+                    <Image
+                      src={listElement.image[0].publicURL}
+                      width={0}
+                      height={0}
+                      sizes="100vw"
+                      className="max-h-full max-w-full object-contain h-auto w-auto"
+                      alt="placeholder"
+                    />
+                  </div>
+                )}
               </a>
             </li>
           ))}
         </div>
-        <button className="border border-black text-2xl px-2.5 py-0.5 m-10" onClick={() => router.push("/newPost")}>
+        <button
+          className="border border-black text-2xl px-2.5 py-0.5 m-10 bg-blue-500 text-white rounded"
+          onClick={() => router.push("/newPost")}
+        >
           글쓰기
         </button>
       </div>
