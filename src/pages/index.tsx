a931d4697baa5b4a3eb66ced42258b146b06c7b3
Yechan Jung <asdxcv1998@gmail.com>
2024-03-05 03:35:29 +0900

    feat: 로고 및 간격 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,38 +30,46 @@ export default function Page() {
   return (
     <>
       <div className="mx-auto bg-gray-100 min-h-screen p-10">
-        <h1 className="text-3xl pb-4 text-blue-500">Logo Here</h1>
-        <div>
-          {listElements.map((listElement: ListElement) => (
-            <li
-              className="border border-black list-none p-2 flex justify-between bg-white mb-4 rounded shadow"
-              key={listElement.id}
+        <div className="flex">
+          <div className="w-40">
+            <div className="mb-10"></div>
+          </div>
+          <div>
+            <Image src={"/logo.png"} width={200} height={100} alt="logo" />
+            {listElements.map((listElement: ListElement) => (
+              <li
+                className="border border-black list-none p-2 flex justify-between bg-white mb-4 rounded shadow"
+                key={listElement.id}
+              >
+                <a className="w-full text-gray-700" onClick={() => goPost(listElement.id)}>
+                  <h2 className="text-xl">{listElement.title}</h2>
+                  <p className="text-sm text-gray-500">{formatTimeAgo(listElement.created_at ?? "")}</p>
+                  {listElement.image.length > 0 && (
+                    <div className="flex justify-center">
+                      <Image
+                        src={listElement.image[0].publicURL}
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        className="max-h-full max-w-full object-contain h-auto w-auto"
+                        alt="placeholder"
+                      />
+                    </div>
+                  )}
+                </a>
+              </li>
+            ))}
+            <button
+              className="border border-black text-2xl px-4 bg-blue-500 text-white rounded"
+              onClick={() => router.push("/newPost")}
             >
-              <a className="w-full text-gray-700" onClick={() => goPost(listElement.id)}>
-                <h2 className="text-xl">{listElement.title}</h2>
-                <p className="text-sm text-gray-500">{formatTimeAgo(listElement.created_at ?? "")}</p>
-                {listElement.image.length > 0 && (
-                  <div className="flex justify-center">
-                    <Image
-                      src={listElement.image[0].publicURL}
-                      width={0}
-                      height={0}
-                      sizes="100vw"
-                      className="max-h-full max-w-full object-contain h-auto w-auto"
-                      alt="placeholder"
-                    />
-                  </div>
-                )}
-              </a>
-            </li>
-          ))}
+              글쓰기
+            </button>
+          </div>
+          <div className="w-40">
+            <div className="mb-10"></div>
+          </div>
         </div>
-        <button
-          className="border border-black text-2xl px-2.5 py-0.5 m-10 bg-blue-500 text-white rounded"
-          onClick={() => router.push("/newPost")}
-        >
-          글쓰기
-        </button>
       </div>
     </>
   );
